48e9fd5d4e80633de39dc31d0caf5c21e7c704c1
Sylvie <sylvie@Sylvie.local>
2022-01-22 18:35:56 +0800

    Add database tables

---
+++ prisma/migrations/20220122040810_add_user_metadata/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "deletedAt" TIMESTAMP(3),
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
