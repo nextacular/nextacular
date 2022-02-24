4f02a3b1e9d5e15471b9e4c29fe579894445045a
Sylvie <sylvie@Sylvie.local>
2022-02-24 11:26:03 +0800

    Add Google analytics tracking code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Router from 'next/router';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
+import ReactGA from 'react-ga';
 import TopBarProgress from 'react-topbar-progress-indicator';
 import { SWRConfig } from 'swr';
 
@@ -13,11 +14,30 @@ import '@/styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
   const [progress, setProgress] = useState(false);
+  const router = useRouter();
   const swrOptions = swrConfig();
 
   Router.events.on('routeChangeStart', () => setProgress(true));
   Router.events.on('routeChangeComplete', () => setProgress(false));
   TopBarProgress.config(progressBarConfig());
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID);
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ReactGA.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <SessionProvider session={pageProps.session}>
