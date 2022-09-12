9800e051133d8b9426f4198e8c153a0a1af11630
Madhur <mformadhur@gmail.com>
2022-09-11 20:07:18 -0700

    Use getSession's status within layouts
    - Load or re-direct pages based on getSession()->status instead of data

--- src/layouts/AccountLayout.js
+++ src/layouts/AccountLayout.js
@@ -10,16 +10,17 @@ import menu from '@/config/menu/index';
 import { useWorkspace } from '@/providers/workspace';
 
 const AccountLayout = ({ children }) => {
-  const { data } = useSession();
+  const { status } = useSession();
   const router = useRouter();
   const { workspace } = useWorkspace();
 
   useEffect(() => {
-    if (!data) {
+    if (status === 'unauthenticated') {
       router.replace('/auth/login');
     }
-  }, [data, router]);
+  }, [status, router]);
 
+  if (status === 'loading') return <></>;
   return (
     <main className="relative flex flex-col w-screen h-screen space-x-0 text-gray-800 dark:text-gray-200 md:space-x-5 md:flex-row bg-gray-50 dark:bg-gray-800">
       <Sidebar menu={menu(workspace?.slug)} />
